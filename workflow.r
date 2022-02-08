fcf138b2862092d6207580fa12f8ac1f07869b92
Benjamin Crary <benjamin.crary@gmail.com>
2022-02-08 10:44:24 -0600

    updated workflow file

--- workflow.r
+++ workflow.r
@@ -26,11 +26,11 @@ my_criteria_results <- filter_by_date(criteria_results, "01/01/1990", "06/30/202
 
 
 #5. Basic Summary
-my_basic_summary <- summarize_basic(my_criteria_results, site_summary_segment, group_lower)
+my_basic_summary <- summarize_basic(my_criteria_results)
 
 
 #6. Last 5 Year Summary
-my_basic_summary_recent <- summarize_basic_recent(criteria_results = my_criteria_results, start_date = "07/01/2016", end_date = "06/30/2021", site_summary_segment, group_lower)
+my_basic_summary_recent <- summarize_basic_recent(criteria_results = my_criteria_results, start_date = "07/01/2016", end_date = "06/30/2021")
 
 
 #7. Exceedance summary, 2016-2021
@@ -43,12 +43,12 @@ my_formatted_period <- format_period_summary(my_period_summary_2016_2021)
 
 #9. consolidate for Appendix B - Class C
 my_appendix_b_class_c <- create_ir_appendix_b_class_c(my_basic_summary, my_basic_summary_recent, my_formatted_period)
-write.csv(my_appendix_b_class_c, file = "output/appendix_b_class_c.csv")
+write.csv(my_appendix_b_class_c, file = "output/appendix_b_class_c.csv", row.names = F)
 
 
 #10. consolidate for Appendix B - Class D
 my_appendix_b_class_d <- create_ir_appendix_b_class_d(my_basic_summary, my_basic_summary_recent, my_formatted_period)
-write.csv(my_appendix_b_class_d, file = "output/appendix_b_class_d.csv")
+write.csv(my_appendix_b_class_d, file = "output/appendix_b_class_d.csv", row.names = F)
 
 
 
