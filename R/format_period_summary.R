7128e431933070b825d471f3a5828a0452a1fa21
Shanna Rucker <42379382+shannarucker@users.noreply.github.com>
2022-12-28 16:32:58 -0600

    Updates to format_period_summary.R

--- R/format_period_summary.R
+++ R/format_period_summary.R
@@ -14,29 +14,29 @@ format_period_summary <- function(period_summary) {
   df_summary_ccc <- df %>%
     dplyr::mutate(start_year = lubridate::year(start_date),
                   end_year = lubridate::year(end_date)) %>%
-    dplyr::filter(evidence_based_exceedance_ccc_3yr_sum > 1) %>%
-    dplyr::group_by(site_summary_segment, group_lower) %>%
+    dplyr::filter(exceedance_ccc_3yr_sum > 1) %>%
+    dplyr::group_by(waterbody_segment, pollutant_group) %>%
     dplyr::summarize(most_recent_period_with_multiple_ccc_exceedances = paste0(max(end_year)-3, " - ", max(end_year)))
 
 
   df_summary_cmc <- df %>%
     dplyr::mutate(start_year = lubridate::year(start_date),
                   end_year = lubridate::year(end_date)) %>%
-    dplyr::filter(evidence_based_exceedance_cmc_3yr_sum > 1) %>%
-    dplyr::group_by(site_summary_segment, group_lower) %>%
+    dplyr::filter(exceedance_cmc_3yr_sum > 1) %>%
+    dplyr::group_by(waterbody_segment, pollutant_group) %>%
     dplyr::summarize(most_recent_period_with_multiple_cmc_exceedances = paste0(max(end_year)-3, " - ", max(end_year)))
 
 
   df_summary_d <- df %>%
     dplyr::mutate(start_year = lubridate::year(start_date),
                   end_year = lubridate::year(end_date)) %>%
-    dplyr::filter(evidence_based_exceedance_d_3yr_sum > 1) %>%
-    dplyr::group_by(site_summary_segment, group_lower) %>%
+    dplyr::filter(exceedance_d_3yr_sum > 1) %>%
+    dplyr::group_by(waterbody_segment, pollutant_group) %>%
     dplyr::summarize(most_recent_period_with_multiple_d_exceedances = paste0(max(end_year)-3, " - ", max(end_year)))
 
 
-  df_summary <- full_join(df_summary_ccc, df_summary_cmc)
+  df_summary <- dplyr::full_join(df_summary_ccc, df_summary_cmc)
 
-  df_summary <- full_join(df_summary, df_summary_d)
+  df_summary <- dplyr::full_join(df_summary, df_summary_d)
 
 }
