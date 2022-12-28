e7008dcbab14a992f7ec25d0bf0ea73e5207d2e3
Shanna Rucker <42379382+shannarucker@users.noreply.github.com>
2022-12-28 12:20:09 -0600

    Update read_ir_data.R. Make sample_date a date field

--- R/read_ir_data.R
+++ R/read_ir_data.R
@@ -10,13 +10,13 @@ read_ir_data <- function(filename) {
 
 #### Read in levels ####
 
-  df <- read.csv(filename)
+  df <- read.csv(filename) %>%
     # dplyr::mutate(lower = group) %>%
     # dplyr::left_join(paramlevels, by = "lower") %>%
     # dplyr::select(-group) %>%
     # dplyr::rename("group_lower" = lower, "group_upper" = upper) %>%
     # tidyr::drop_na(group_upper) %>%
-    # dplyr::mutate(date = as.Date(date, format="%m/%d/%Y"))
+    dplyr::mutate(sample_date = as.Date(sample_date))
 
   return(df)
 
