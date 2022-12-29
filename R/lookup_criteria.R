3198b7c1c6952fc77aaf53d52e32132f00c186a9
Shanna Rucker <42379382+shannarucker@users.noreply.github.com>
2022-12-29 15:02:18 -0500

    Fix mismatch in waterbody names and pollutant names within criteria tables

--- R/lookup_criteria.R
+++ R/lookup_criteria.R
@@ -8,11 +8,23 @@
 #' @examples
 lookup_criteria <- function(df) {
 
+  # Expand non_metals_criteria table so that there is a record for pollutant/waterbody combination
+  my_non_metals_criteria <- expand.grid(waterbody_segment = waterbodies$waterbody_segment, pollutant_name = non_metals_criteria$pollutant_name )
+
+  my_non_metals_criteria <- my_non_metals_criteria %>%
+    dplyr::left_join(non_metals_criteria, by = "pollutant_name")
+
+
+  # Combine metal and non-metal criteria
+  all_criteria <- dplyr::bind_rows(metals_criteria, my_non_metals_criteria)
+
+
+  # Join criteria to main dataframe
   df <- df %>%
     dplyr::left_join(all_criteria,
-                     by = c("waterbody_segment", "pollutant_name")) %>%
-    dplyr::mutate(ccc = as.numeric(ccc),
-           cmc = as.numeric(cmc),
-           d = as.numeric(d))
+                     by = c("waterbody_segment", "pollutant_name"))
+    # dplyr::mutate(ccc = as.numeric(ccc),
+    #        cmc = as.numeric(cmc),
+    #        d = as.numeric(d))
 
 }
